c936bc235009d503614f454836b37ad2943e6e62
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2019-03-24 20:02:10 -0300

    changing the ipc communication to ajax request to flask server

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
+// electron objects
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require ('path');
 const fs = require('fs');
 const os = require('os');
 
+//  TODO: open backend flask server dynamic to open port
 
 let mainWindow = null;
 
 const createWindow = () => {
+  // var subpy = require('child_process').spawn('python3', ['./image_augmentator/server.py']);
+  const rq = require('request-promise');
+  
   mainWindow = new BrowserWindow({ width: 800, height: 600 })
   mainWindow.loadURL(require('url').format({
     pathname: path.join(__dirname, 'app/index.html'),
@@ -15,32 +20,32 @@ const createWindow = () => {
   }))
   mainWindow.webContents.openDevTools()
   mainWindow.maximize()
-  let contents = mainWindow.webContents;
   mainWindow.on('closed', () => {
     mainWindow = null
+    subpy.kill('SIGINT')
   })
-  
 }
 
-ipcMain.on('form-submission', function (event, firstname) {
-  console.log("this is the firstname from the form ->", firstname)
-});
+// // MANDAR THREAD NO PYTHON-SHELL PARA A AUGMENTATION
+// ipcMain.on('form-submission', function (event, args) {
+//   // CODE HERE
+// });
 
-ipcMain.on('teste-disney', function (event, oxi) {
-  console.log(oxi)
-});
+// // SUBMIT DO FORM
+// ipcMain.on('form-submission', function (event, args) {
+//   console.log("this is the firstname from the form ->", args)
+// });
+
+// // SALVAR O JSON COM AS CONFIGURAÇÕES
+// ipcMain.on('save-json-configs', function (event, json) {
+//   //  WRITE JSON INTO __DIRNAME/CONFIGS
+// });
 
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
   }
 })
