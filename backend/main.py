4f283fede38bc38799545d0958f26e3ac0899fb5
Jonatha Varjão <jonathavarjao@urpay.com.br>
2019-08-18 23:38:37 -0300

    adding cors to localhost react

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
-
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI('Image++')
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http:localhost",
+    "http:localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 """
     Rotas da API com as chamadas em AJAX (front)
 """
