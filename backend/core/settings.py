f5973ba09fa56be14715f93286d17a5f891f2bb7
Jonatha Varjão <jonathavarjao@urpay.com.br>
2019-08-19 00:17:36 -0300

    adding env variables

---
+++ backend/core/settings.py
@@ -0,0 +1,26 @@
+import os
+
+
+def getenv_boolean(var_name, default_value=False):
+    result = default_value
+    env_value = os.getenv(var_name)
+    if env_value is not None:
+        result = env_value.upper() in ("TRUE", "1")
+    return result
+
+
+API_V1_STR = "/api/v1"
+
+SECRET_KEY = os.getenvb(b"SECRET_KEY")
+if not SECRET_KEY:
+    SECRET_KEY = os.urandom(32)
+"""
+    SWAP PSQL to SQlite
+"""
+# POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
+# POSTGRES_USER = os.getenv("POSTGRES_USER")
+# POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+# POSTGRES_DB = os.getenv("POSTGRES_DB")
+# SQLALCHEMY_DATABASE_URI = (
+#     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"
+# )
